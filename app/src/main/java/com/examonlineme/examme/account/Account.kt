99b9476de79a597e054e324f3fb708837a387a4d
dipankar108 <nathbd10@gmail.com>
2022-04-01 13:19:35 +0600

    fragment added for registerUser

--- app/src/main/java/com/examonlineme/examme/account/Account.kt
+++ app/src/main/java/com/examonlineme/examme/account/Account.kt
@@ -1,7 +1,8 @@
-package com.examonlineme.examme
+package com.examonlineme.examme.account
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.examonlineme.examme.R
 
 class Account : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
