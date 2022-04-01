2a0c1693f2872bfd8258c04fe0b117874a5829b3
dipankar108 <nathbd10@gmail.com>
2022-04-01 13:16:13 +0600

    New activity added

---
+++ app/src/main/java/com/examonlineme/examme/Account.kt
@@ -0,0 +1,11 @@
+package com.examonlineme.examme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
